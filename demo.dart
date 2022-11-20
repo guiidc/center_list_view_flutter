d6bc744fc631e33932f7a8abef95edddbfcb379e
Guilherme C <82621852+guiidc@users.noreply.github.com>
2022-11-20 19:13:52 -0300

    Create demo.dart

---
+++ demo.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'My Flutter App',
+      debugShowCheckedModeBanner: false, // Remove debug banner
+      home: Scaffold(
+          appBar: AppBar(
+            // The title text which will be shown on the action bar
+            title: Text("My Flutter App"),
+          ),
+          body: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.amber,
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.amber,
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
